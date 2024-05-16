8990e7284159fc3a22ff17799a627bf7276b917d
Viorel Cojocaru <vio@beanon.com>
2024-05-16 12:51:46 +0200

    Notes

--- apps/nextjs/src/app/page.js
+++ apps/nextjs/src/app/page.js
@@ -5,11 +5,16 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
-        <h2>App component</h2>
-        <Button>Action</Button>
+      <div className={styles.content}>
+        <p>Button component re-exported from library:</p>
+        <ul>
+          <li>🟢 when the CSS order is correct</li>
+          <li>🔴 when the CSS order is incorrect</li>
+        </ul>
+        <div className={styles.preview}>
+          <Button>Button</Button>
+        </div>
       </div>
-
     </main>
   );
 }
